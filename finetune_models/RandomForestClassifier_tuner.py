13f3b42d81c1386fb6861c08e34dd886d097012d
Aryan Chauhan <23f3003877@ds.study.iitm.ac.in>
2026-05-02 05:51:55 +0000

    tried to fix ram issue for randomforest

--- finetune_models/RandomForestClassifier_tuner.py
+++ finetune_models/RandomForestClassifier_tuner.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 import optuna
+import gc
 
 def build_tuned_RandomForestClassifier(x, y, n_trials=50):
     """
@@ -15,7 +16,7 @@ def build_tuned_RandomForestClassifier(x, y, n_trials=50):
     """
     def objective(trial):
         params = {
-            'n_estimators': trial.suggest_int('n_estimators', 50, 2000),
+            'n_estimators': trial.suggest_int('n_estimators', 50, 1000),
             'max_depth': trial.suggest_categorical('max_depth', [None, 10, 20, 30, 40, 50]),
             'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
             'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 15),
@@ -29,6 +30,15 @@ def build_tuned_RandomForestClassifier(x, y, n_trials=50):
         cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
         score = cross_val_score(model, x, y, cv=cv, scoring='f1_weighted', n_jobs=1).mean()
+
+        # --- MEMORY CLEANUP BLOCK ----
+        del X_train, y_train, X_val, y_val
+        gc.collect() 
+
+        # 3. Nuke the massive model object before the next Optuna trial
+        del model
+        gc.collect()
+
         return score
 
     study = optuna.create_study(direction='maximize', study_name="RF_Tuning")
